adf29de6578161eda0d82ef21762efdd2af5c76f
Mohamed Nasser M <83620603+Mohamed-Nasser-M@users.noreply.github.com>
2023-09-28 19:17:31 +0300

    Update Linked_Queue.h

--- Queue/Linked_Queue.h
+++ Queue/Linked_Queue.h
@@ -1,1 +1,18 @@
-
+#define queue_entry int
+struct QueueNode{
+    queue_entry entry;
+    struct QueueNode *next;
+};
+typedef struct queue{
+    struct QueueNode *front;
+    struct QueueNode *rear;
+    int size;
+}linked_Queue;
+void creat_queue    (linked_Queue*);
+void append   (queue_entry,linked_Queue*);
+void serve    (linked_Queue*);
+int queue_empty  (linked_Queue*);
+int queue_full   (linked_Queue*);
+int queue_size   (linked_Queue*);
+void clear_queue (linked_Queue*);
+void traverse_queue  (linked_Queue*,void(*)(queue_entry));
